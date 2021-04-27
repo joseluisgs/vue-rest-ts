73531570a6ef32edcd059f475fe240d2b16472be
joseluisgs <joseluisgonsan@gmail.com>
2021-04-27 17:26:12 +0200

    Servicio esqueleto

---
+++ src/assets/services/users.ts
@@ -0,0 +1,15 @@
+// Cliente de la Api Rest
+export default {
+  findAll() {
+    // Método para obtener la lista de usuarios
+  },
+  save() {
+    // Método para crear un usuario
+  },
+  update() {
+    // Método para actualizar un usuario
+  },
+  remove() {
+    // Método para borrar un usuario
+  },
+};
