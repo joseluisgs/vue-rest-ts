b6f58f53400a6c37e749a0ec16241c2bf5e8ca80
joseluisgs <joseluisgonsan@gmail.com>
2021-04-27 18:11:20 +0200

    User Service

--- src/services/users.ts
+++ src/services/users.ts
@@ -2,19 +2,27 @@
 import axios from 'axios';
 import IUser from '@/interfaces/IUser';
 
-const URL = 'https://jsonplaceholder.typicode.com/users';
+const URL = 'https://jsonplaceholder.typicode.com/users/';
 
 export default {
+  /**
+   * Obtiene el listado de usuarios
+   * @returns Lista de Usuarios
+   */
   async findAll(): Promise<IUser[]> {
-    return (await axios.get(URL)).data;
+    const response = await axios.get(URL);
+    return response.data;
   },
-  save() {
-    // Método para crear un usuario
+
+  async save(user: IUser): Promise<IUser> {
+    return (await axios.post(URL, user)).data;
   },
-  update() {
-    // Método para actualizar un usuario
+
+  async update(user: IUser): Promise<IUser> {
+    return (await axios.put(URL + user.id, user)).data;
   },
-  remove() {
-    // Método para borrar un usuario
+
+  async remove(user: IUser): Promise<IUser> {
+    return (await axios.delete(URL + user.id)).data;
   },
 };
