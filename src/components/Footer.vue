acaba70b0f077c928e7b50243a4ca98990137537
joseluisgs <joseluisgonsan@gmail.com>
2021-04-28 13:04:33 +0200

    Refactor

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,23 +1,24 @@
 <template>
   <footer id="Footer" class="footer">
-        <div class="content has-text-centered is-size-7-mobile">
-            <p class="copyright">Coding with <i class="mdi mdi-heart-multiple has-text-danger" size="is-small"></i> &copy;
-                <a href="http://github.com/joseluisgs" target="_blank" rel="noopener noreferrer">José Luis González
-                    Sánchez</a>. 2021
-            </p>
-            <a href="https://twitter.com/joseluisgonsan" target="_blank">
-                <i class="mdi mdi-24px mdi-twitter has-text-twitter"></i>
-            </a>
-            <a href="https://github.com/joseluisgs" target="_blank">
-                <i class="mdi mdi-24px mdi-github has-text-github"></i>
-            </a>
-            <a href="https://www.linkedin.com/in/joseluisgonsan/?locale=es_ES" target="_blank">
-                <i class="mdi mdi-24px mdi-linkedin has-text-linkedin"></i>
-            </a>
-        </div>
-    </footer>
+    <div class="content has-text-centered is-size-7-mobile">
+      <p class="copyright">
+        Coding with <i class="mdi mdi-heart-multiple has-text-danger" size="is-small"></i> &copy;
+        <a href="http://github.com/joseluisgs" target="_blank" rel="noopener noreferrer"
+          >José Luis González Sánchez</a
+        >. 2021
+      </p>
+      <a href="https://twitter.com/joseluisgonsan" target="_blank">
+        <i class="mdi mdi-24px mdi-twitter has-text-twitter"></i>
+      </a>
+      <a href="https://github.com/joseluisgs" target="_blank">
+        <i class="mdi mdi-24px mdi-github has-text-github"></i>
+      </a>
+      <a href="https://www.linkedin.com/in/joseluisgonsan/?locale=es_ES" target="_blank">
+        <i class="mdi mdi-24px mdi-linkedin has-text-linkedin"></i>
+      </a>
+    </div>
+  </footer>
 </template>
 
 <style lang="scss">
-
 </style>
